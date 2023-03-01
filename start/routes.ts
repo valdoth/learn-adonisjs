da7a31b5f27e39c721c94e00f2cf773f81fce19a
valdoth <valdotsiarohasina@gmail.com>
2023-03-01 15:02:13 +0300

    create adonis project

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,27 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
+
+// Route.get('/news', ({ view }) => {
+//   return view.render('news/view');
+// })
+Route.on("/news").render("news.view").as('news.view');
+
+Route.post('/news', ({ response }) => {
+  // const { email, password } = request.body();
+  return response.redirect('/news');
+}).as('news.create')
+
+Route.patch('/news/:id', ({ params }) => {
+  return { params };
+}).where('id', {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id)
+}).as('news.update')
+
+Route.delete('/news/:id', ({ params }) => {
+  return { params };
+}).where('id', {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id)
+}).as('news.delete')
